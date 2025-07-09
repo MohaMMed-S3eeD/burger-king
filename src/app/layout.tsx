18091b14e9f5cb03a8bad2d9077f1345263a0864
Mohamed Seed <mosaeed7888@gmail.com>
2025-07-09 17:25:04 +0300

    init Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const flameBold = localFont({
+  src: "../fonts/FlameBold.otf",
+  variable: "--font-flame-bold",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -24,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${flameBold.variable} antialiased`}>{children}</body>
     </html>
   );
 }
